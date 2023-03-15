eb4513242ac3b0fd5d81d642cb5a17b4bd157980
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-15 12:25:29 -0700

    completed other exercises

--- app.js
+++ app.js
@@ -27,3 +27,44 @@ function checkAge(userName, age) {
 }
 
 checkAge("Jeff", 15);
+
+//Exercise 3 Section
+console.log("EXERCISE 3:\n==========\n");
+
+function cartesian(x, y) {
+  let origin = "This point is at the origin.";
+  let quad1 = "This point is in quadrant 1.";
+  let quad2 = "This point is in quadrant 2.";
+  let quad3 = "This point is in quadrant 3.";
+  let quad4 = "This point is in quadrant 4.";
+  if (x == 0 && y == 0) {
+    console.log(origin);
+  } else if (x > 0 && y > 0) {
+    console.log(quad1);
+  } else if (x < 0 && y > 0) {
+    console.log(quad2);
+  } else if (x < 0 && y < 0) {
+    console.log(quad3);
+  } else {
+    console.log(quad4);
+  }
+}
+
+cartesian(1, -4);
+
+//Exercise 4 Section
+console.log("EXERCISE 4:\n==========\n");
+
+function triangle(a, b, c) {
+  if (a + b <= c || a + c <= b || b + c <= a) {
+    console.log("This is an invalid triangle");
+  } else if (a == b && b == c) {
+    console.log("Equilateral");
+  } else if (a == b || a == c || b == c) {
+    console.log("Isosceles");
+  } else {
+    console.log("Scalene");
+  }
+}
+
+triangle(1, 1, 1);
