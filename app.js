5b49f475e8f16960009939b13fb0cb7f9ade29ba
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-15 10:13:09 -0700

    did first two exercises

--- app.js
+++ app.js
@@ -3,5 +3,27 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+function printOdds(count) {
+  for (let i = 0; i <= count; i++) {
+    if (i % 2 == 1) {
+      console.log(i);
+    }
+  }
+}
+
+printOdds(51);
+
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+
+function checkAge(userName, age) {
+  let aboveSixteen = `Congrats ${userName}, you can drive!`;
+  let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16.`;
+  if (age < 16) {
+    console.log(belowSixteen);
+  } else {
+    console.log(aboveSixteen);
+  }
+}
+
+checkAge("Jeff", 15);
